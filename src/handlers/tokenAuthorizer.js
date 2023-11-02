de968b514acf616cabd666f0c332b531ac489f05
Galeno de Melo <galenodemelo@gmail.com>
2023-11-02 16:54:28 -0300

    feat: Autentica com base no CPF

--- src/handlers/tokenAuthorizer.js
+++ src/handlers/tokenAuthorizer.js
@@ -1,11 +1,28 @@
 const { allow, deny} = require("../lib/policy")
+const { getConnection } = require("../lib/database");
 
 exports.handler = async (event) => {
+    let connection = null
+
     try {
         if (!event.type == "TOKEN") throw new Error(`Evento do tipo [${event.type}] não suportado`)
-        return allow("customer-cpf", event.methodArn)
+
+        const token = event.authorizationToken?.trim()
+        if (!token) throw new Error("Obrigatório informar um token")
+
+        connection = await getConnection()
+        const [rows] = await connection.query("SELECT id FROM customer_ca WHERE cpf = ? LIMIT 1", [token])
+        if (!rows.length) {
+            console.error(`Cliente com CPF [${token}] não encontrado`)
+            return deny(token, event.methodArn)
+        }
+
+        const customerId = rows[0].id.toString()
+        return allow(customerId, event.methodArn)
     } catch (error) {
         console.error("Erro inesperado", error)
-        return deny(event.methodArn)
+        return deny("", event.methodArn)
+    } finally {
+        if (connection) connection.end()
     }
 }
