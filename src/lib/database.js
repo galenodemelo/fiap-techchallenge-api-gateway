d0e8ad41c13dfb1cf6d5814f3d4ca5570f58ac06
Galeno de Melo <galenodemelo@gmail.com>
2023-11-02 16:53:52 -0300

    feat: Conecta ao banco MySQL

---
+++ src/lib/database.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2/promise")
+
+exports.getConnection = async () => {
+    return await mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME
+    })
+}
